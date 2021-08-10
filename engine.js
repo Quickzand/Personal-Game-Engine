ee542c204f44fb2e61e28eb0289b8e67f9cd5413
Quickzand <matthewsand22@gmail.com>
2021-08-10 03:58:10 -0400

    Added in basic kinematics and collisions

--- engine.js
+++ engine.js
@@ -5,11 +5,17 @@ class GameEngine {
     constructor(canvasID) {
         this.canvas = document.getElementById(canvasID)
         this.ctx = this.canvas.getContext("2d")
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
         this.startLoop();
     }
 
     addEntity(entity) {
         this.entityList.push(entity);
+        entity.canvas = this.canvas;
+        entity.ctx = this.ctx;
+        entity.gameEngine = this;
+
     }
     removeEntity(entity) {
         this.entityList.splice(this.entityList.indexOf(entity), 1);
@@ -19,6 +25,17 @@ class GameEngine {
             entity.update();
         });
     }
+
+    checkCollisions(collider) {
+        for (var i in this.entityList) {
+            var otherEntity = this.entityList[i];
+            if (collider.entity !== otherEntity && otherEntity.collider && collider.checkCollision(otherEntity)) {
+                return true
+            }
+        }
+        return false
+    }
+
 
     draw() {
         this.entityList.forEach(entity => {
@@ -49,6 +66,21 @@ class GameEngine {
 class Entity {
     dx = 0
     dy = 0
+    ddx = 0
+    ddy = 0
+    gravity = 0.1
+    jumpHeight = 1
+    jumpVelocity = 0.5
+    hasGravity = true
+    terminalVelocity = 2
+    onGround = false
+    canvas = null
+    ctx = null
+    collider = null;
+
+
+
+
 
 
     constructor(x, y, width, height) {
@@ -56,6 +88,7 @@ class Entity {
         this.y = y
         this.width = width
         this.height = height
+        this.collider = new Collider(this)
     }
 
     draw(ctx) {
@@ -63,6 +96,46 @@ class Entity {
     }
 
     update() {
+
+        this.collider.update()
+        this.onGround = false;
+        if (this.x < 0) {
+            this.x = 0
+        }
+        if (this.x > this.canvas.width - this.width) {
+            this.x = this.canvas.width - this.width
+        }
+        if (this.y < 0) {
+            this.y = 0
+        }
+        if (this.y > this.canvas.height - this.height) {
+            this.y = this.canvas.height - this.height
+            this.onGround = true
+        }
+        this.dx += this.ddx
+        this.dy += this.ddy
+
+
+
+        this.dx = this.dx > this.terminalVelocity ? this.terminalVelocity : this.dx
+        this.dx = this.dx < -this.terminalVelocity ? -this.terminalVelocity : this.dx
+        this.dy = this.dy > this.terminalVelocity ? this.terminalVelocity : this.dy
+        this.dy = this.dy < -this.terminalVelocity ? -this.terminalVelocity : this.dy
+        this.ddx = 0
+        this.ddy = 0
+        if (this.hasGravity && !this.onGround) {
+            this.dy += this.gravity
+        }
+
+        // Checks if new position collides with any other entity based on x velocity and if it does, x velocity goes to 0 
+        if (Math.abs(this.dx) > 0 && this.gameEngine.checkCollisions(this.collider.createTheoriticalBoundingBox(this.collider.x + this.dx, this.collider.y))) {
+            this.dx = 0;
+        }
+        // Checks if new position collides with any other entity based on y velocity and if it does, y velocity goes to 0
+        if (Math.abs(this.dy) > 0 && this.gameEngine.checkCollisions(this.collider.createTheoriticalBoundingBox(this.collider.x, this.collider.y + this.dy))) {
+            this.dy = 0;
+            this.onGround = true;
+        }
         this.x += this.dx
         this.y += this.dy
         return this
@@ -73,6 +146,100 @@ class Entity {
         this.dy = dy
         return this
     }
+
+    setXVelocity(dx) {
+        this.dx = dx
+        return this
+    }
+    setYVelocity(dy) {
+        this.dy = dy
+        return this
+    }
+
+    setX(x) {
+        this.x = x
+        return this
+    }
+    setY(y) {
+        this.y = y
+        return this
+    }
+
+    // Adds a force to the entity in the given directions
+    addForces(ddx, ddy) {
+        this.ddx += ddx
+        this.ddy += ddy
+        return this
+    }
+    // Adds force in the X direction
+    addXForce(ddx) {
+        this.ddx += dx
+        return this
+    }
+    // Adds force in the Y direction
+    addYForce(ddy) {
+        this.ddy += ddy
+        return this
+    }
+
+    // Returns entity's center
+    getCenter() {
+        return {
+            x: this.x + this.width / 2,
+            y: this.y + this.height / 2
+        }
+    }
+
+    // Returns the entity's bounding box
+    getBoundingBox() {
+        return new BoundingBox(this.x, this.y, this.width, this.height)
+    }
+}
+
+
+
+
+class BoundingBox {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+
+    checkCollision(checkBoundingBox) {
+        return (this.x < checkBoundingBox.x + checkBoundingBox.width &&
+            this.x + this.width > checkBoundingBox.x &&
+            this.y < checkBoundingBox.y + checkBoundingBox.height &&
+            this.y + this.height > checkBoundingBox.y)
+    }
+
+    getCenter() {
+        return {
+            x: this.x + this.width / 2,
+            y: this.y + this.height / 2
+        }
+    }
+}
+
+class Collider extends BoundingBox {
+    constructor(entity) {
+        super(entity.x, entity.y, entity.width, entity.height)
+        this.entity = entity;
+    }
+
+    // Returns a new theoritical bounding box used for collision detection
+    createTheoriticalBoundingBox(x, y) {
+        var temp = new BoundingBox(x, y, this.width, this.height)
+        temp.entity = this.entity;
+        return temp;
+    }
+
+    update() {
+        this.x = this.entity.x
+        this.y = this.entity.y
+    }
+
 }
 
 
@@ -112,29 +279,72 @@ class Circle extends Entity {
 }
 
 class Player extends Rectangle {
+    playerSpeed = 5
+    canJump = true
+
     constructor(x, y, width, height, color) {
         super(x, y, width, height, color)
         window.addEventListener("keydown", (e) => {
-            console.log(e.key)
             if (e.key == "a") {
-                this.setVelocity(-5, 0)
+                this.leftDown = true;
             }
             if (e.key == "w") {
-                this.setVelocity(0, -5)
+                this.upDown = true;
             }
             if (e.key == "d") {
-                this.setVelocity(5, 0)
+                this.rightDown = true;
             }
             if (e.key == "s") {
-                this.setVelocity(0, 5)
+                this.downDown = true;
+            }
+        }, false)
+        window.addEventListener("keyup", (e) => {
+            if (e.key == "a") {
+                this.leftDown = false;
+            }
+            if (e.key == "w") {
+                this.upDown = false;
+            }
+            if (e.key == "d") {
+                this.rightDown = false;
+            }
+            if (e.key == "s") {
+                this.downDown = false;
             }
         }, false)
     }
 
 
+    // If the movement buttons are pressed down move the player in that direction
+    playerControls(canvas) {
+        if (this.leftDown || this.rightDown) {
+            this.setXVelocity(0)
+        }
+        if (this.rightDown && this.x < this.canvas.width - this.width) {
+            this.x += this.playerSpeed;
+        }
+        if (this.leftDown && this.x > 0) {
+            this.x -= this.playerSpeed;
+        }
+        if (this.upDown && this.y > 0 && this.canJump) {
+            this.addYForce(-this.jumpVelocity);
+            this.canJump = false;
+        }
+        if (this.downDown && this.y < this.canvas.height - this.height) {
+            this.y += this.playerSpeed;
+        }
+        this.onGround ? this.canJump = true : null;
+        return this
+    }
+
+    update(canvas) {
+        this.playerControls(canvas);
+        super.update()
+    }
 }
 
 
 
 test = new GameEngine(canvasId)
-test.addEntity(new Player(0, 0, 10, 10, "#FFF").setVelocity(1, 1))
+test.addEntity(new Player(0, 0, 50, 150, "#FFF").setVelocity(1, 0))
+test.addEntity(new Rectangle(50, 500, 500, 50, "#FFF"))
